4755263420ede4520a8c40476ae0a203f28747dd
rafaelpcosta <rafaelpcosta74@gmail.com>
2018-12-16 22:35:10 +0000

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfPN4IUAAAAAG4bWzrR3pS5iz604mq8riH1ky1h',  // <----- EDIT ME!
-  redirect_uri: 'https://rafaelpcosta.github.io/',
+  redirect_uri: 'https://rafaelpcosta.github.io/dice-betting',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
