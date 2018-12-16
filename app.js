b7d4a65de185835adbee6ae52c0eb24c9cd8977f
rafaelpcosta <rafaelpcosta74@gmail.com>
2018-12-16 22:31:24 +0000

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 3013,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'dice-betting',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfPN4IUAAAAAG4bWzrR3pS5iz604mq8riH1ky1h',  // <----- EDIT ME!
